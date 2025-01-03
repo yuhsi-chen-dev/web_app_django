68327fb77d39a72c48d5cbc4ba3ed803e39b76bb
verna0214 <yuhsichen0214@gmail.com>
2025-01-03 14:08:02 +0800

    [NEW] create home page url and function

--- core/urls.py
+++ core/urls.py
@@ -18,6 +18,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-urlpatterns = [
-    path("admin/", admin.site.urls),
-]
+from posts.views import *
+
+urlpatterns = [path("admin/", admin.site.urls), path("", home_view)]
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home_view(request):
+    """
+    View function that renders the index.html template with a dynamic title.
+
+    Args:
+        request: The HTTP request object that contains metadata about the request.
+
+    Returns:
+        HttpResponse: A rendered HTML response with the context data passed to the template.
+    """
+    title = "Welcome to Django"
+    return render(request, "index.html", {"title": title})
