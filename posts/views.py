2eabd80a3bb03cae6cbe4b639c555553495c6f0a
verna0214 <yuhsichen0214@gmail.com>
2025-01-03 14:40:12 +0800

    [REFACTOR] modulize dynamic section from base.html

--- posts/views.py
+++ posts/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 def home_view(request):
     """
-    View function that renders the index.html template with a dynamic title.
+    View function that renders the index.html template.
 
     Args:
         request: The HTTP request object that contains metadata about the request.
@@ -11,5 +11,4 @@ def home_view(request):
     Returns:
         HttpResponse: A rendered HTML response with the context data passed to the template.
     """
-    title = "Welcome to Django"
-    return render(request, "base.html", {"title": title})
+    return render(request, "posts/home.html")
