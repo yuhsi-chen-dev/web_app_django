a2f78656de06af36dd8f6183b4cc9032028648ca
verna0214 <yuhsichen0214@gmail.com>
2025-01-04 23:46:20 +0800

    [NEW] add post form from django models

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
+from posts.core import PostCreateForm
+
 from .models import *
 
 
@@ -24,17 +26,18 @@ def home_view(request: HttpRequest) -> HttpResponse:
 
 def post_create_view(request: HttpRequest) -> HttpResponse:
     """
-    Render the post creation page.
+    Render the post creation page with a form.
 
-    This view simply renders the `post_create.html` template for creating a new post.
+    This view renders the `post_create.html` template, which contains a form
+    for creating a new post. The form is initialized using the `PostCreateForm`
+    class, which is bound to the `Post` model.
 
     Args:
         request (HttpRequest): The HTTP request object containing metadata about the request.
 
     Returns:
-        HttpResponse: A rendered HTML response for the post creation page.
+        HttpResponse: A rendered HTML response for the post creation page, with the form
+        context included for user input.
     """
-    return render(
-        request,
-        "posts/post_create.html",
-    )
+    form = PostCreateForm()
+    return render(request, "posts/post_create.html", {"form": form})
