ca15eef28142b94fd3451d577352b6d08dbba3fc
verna0214 <yuhsichen0214@gmail.com>
2025-01-09 15:31:17 +0800

    [NEW] add email verification feature

--- core/settings.py
+++ core/settings.py
@@ -140,3 +140,6 @@ MEDIA_ROOT = BASE_DIR / "media"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 LOGIN_REDIRECT_URL = "/"
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+ACCOUNT_AUTHENTICATION_METHOD = "email"
+ACCOUNT_EMAIL_REQUIRED = True
