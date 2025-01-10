2c8d7231debedcb8ffa52ddfdc8c798c48f8f5a3
verna0214 <yuhsichen0214@gmail.com>
2025-01-10 11:05:18 +0800

    [REFACTOR] update templates variables

--- core/urls.py
+++ core/urls.py
@@ -33,6 +33,7 @@ urlpatterns = [
     path("post/edit/<pk>/", post_edit_view, name="post-edit"),
     path("post/<pk>/", post_page_view, name="post"),
     path("profile", profile_view, name="profile"),
+    path("profile/edit", profile_edit_view, name="profile-edit"),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- users/views.py
+++ users/views.py
@@ -5,3 +5,7 @@ from django.shortcuts import render
 def profile_view(request: HttpRequest) -> HttpResponse:
     profile = request.user.profile
     return render(request, "users/profile.html", {"profile": profile})
+
+
+def profile_edit_view(request: HttpRequest) -> HttpResponse:
+    return render(request, "users/profile_edit.html")
