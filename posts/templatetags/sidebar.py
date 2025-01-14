d1280001d84ab978f0ca8c889f6c85c04b460364
verna0214 <yuhsichen0214@gmail.com>
2025-01-14 15:20:29 +0800

    [NEW] add top post templates

--- posts/templatetags/sidebar.py
+++ posts/templatetags/sidebar.py
@@ -1,12 +1,23 @@
+from django.db.models import Count
 from django.template import Library
 
-from ..models import Tag
+from ..models import Post, Tag
 
 register = Library()
 
 
 @register.inclusion_tag("includes/sidebar.html")
-def sidebar_view(tag=None):
+def sidebar_view(tag=None, user=None):
     categories = Tag.objects.all()
-    context = {"categories": categories, "tag": tag}
+    top_posts = (
+        Post.objects.annotate(num_likes=Count("likes"))
+        .filter(num_likes__gt=0)
+        .order_by("-num_likes")
+    )
+    context = {
+        "categories": categories,
+        "tag": tag,
+        "top_posts": top_posts,
+        "user": user,
+    }
     return context
