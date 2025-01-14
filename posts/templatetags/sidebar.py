283653a13c5c7f4a528df08c550d3d84b99e9116
verna0214 <yuhsichen0214@gmail.com>
2025-01-14 15:26:46 +0800

    [NEW] add top comments templates

--- posts/templatetags/sidebar.py
+++ posts/templatetags/sidebar.py
@@ -1,7 +1,7 @@
 from django.db.models import Count
 from django.template import Library
 
-from ..models import Post, Tag
+from ..models import Comment, Post, Tag
 
 register = Library()
 
@@ -14,10 +14,16 @@ def sidebar_view(tag=None, user=None):
         .filter(num_likes__gt=0)
         .order_by("-num_likes")
     )
+    top_comments = (
+        Comment.objects.annotate(num_likes=Count("likes"))
+        .filter(num_likes__gt=0)
+        .order_by("-num_likes")
+    )
     context = {
         "categories": categories,
         "tag": tag,
         "top_posts": top_posts,
         "user": user,
+        "top_comments": top_comments,
     }
     return context
